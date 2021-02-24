a11a0f439f86e87a1238ba8256775340df9229ad
Ayaan A. Ali <ayaan_ali13@hotmail.com>
2021-02-24 10:10:40 +0100

    arbejder på test

--- TestLegacy/UnitTest1.cs
+++ TestLegacy/UnitTest1.cs
@@ -6,72 +6,71 @@ namespace TestLegacy
 {
     public class Tests
     {
-        private FakeECS uut;
         private int _threshold;
-        //public ECS.Legacy.ECS uut;
-        
+        public ECS.Legacy.ECS uut;
+        public ITempSensor tempSensor;
+        public IHeater heater;
 
         [SetUp]
         public void Setup()
         {
-            uut = new FakeECS(_threshold); 
-           // uut = new ECS.Legacy.ECS(_threshold); 
-           
+            uut = new ECS.Legacy.ECS(_threshold, new FakeTempSensor(), new FakeHeater());
         }
 
         [Test]
-        public void Test1()
+        public void Regulate_LowTemp_HeaterIsTurnedOn()
         {
+            tempSensor.ge
             Assert.Pass();
         }
     }
     /// <summary>
     /// Dette er en én til én klasse med ECS hvor vi kan styre dependencies: _threshold og har skiftet TempSensor og Heater klassen ud med vores egne fakes. 
     /// </summary>
-    public class FakeECS : IECS
-    {
-        private int _threshold;
-        //private readonly TempSensor _tempSensor;
-        //private readonly Heater _heater;
-        private FakeTempSensor F_tempSensor;
-        private FakeHeater F_heater;
+    //public class FakeECS : IECS
+    //{
+    //    private int _threshold;
+    //    //private readonly TempSensor _tempSensor;
+    //    //private readonly Heater _heater;
+    //    private FakeTempSensor F_tempSensor;
+    //    private FakeHeater F_heater;
 
-        public FakeECS(int thr)
-        {
-            SetThreshold(thr);
-           // _tempSensor = new TempSensor();
-            //_heater = new Heater();
-            F_tempSensor = new FakeTempSensor();
-            F_heater = new FakeHeater();
+    //    public FakeECS(int thr)
+    //    {
+    //        SetThreshold(thr);
+    //       // _tempSensor = new TempSensor();
+    //        //_heater = new Heater();
+    //        F_tempSensor = new FakeTempSensor();
+    //        F_heater = new FakeHeater();
 
-        }
+    //    }
 
-        public void Regulate()
-        {
-            var t = F_tempSensor.GetTemp();
-            if (t < _threshold)
-                F_heater.TurnOn();
-            else
-                F_heater.TurnOff();
-        }
-        public void SetThreshold(int thr)
-        { _threshold = thr; }
+    //    public void Regulate()
+    //    {
+    //        var t = F_tempSensor.GetTemp();
+    //        if (t < _threshold)
+    //            F_heater.TurnOn();
+    //        else
+    //            F_heater.TurnOff();
+    //    }
+    //    public void SetThreshold(int thr)
+    //    { _threshold = thr; }
 
-        public int GetThreshold()
-        {
-            return _threshold;
-        }
+    //    public int GetThreshold()
+    //    {
+    //        return _threshold;
+    //    }
 
-        public int GetCurTemp()
-        {
-            return F_tempSensor.GetTemp();
-        }
+    //    public int GetCurTemp()
+    //    {
+    //        return F_tempSensor.GetTemp();
+    //    }
 
-        public bool RunSelfTest()
-        {
-            return F_tempSensor.RunSelfTest() && F_heater.RunSelfTest();
-        }
-    }
+    //    public bool RunSelfTest()
+    //    {
+    //        return F_tempSensor.RunSelfTest() && F_heater.RunSelfTest();
+    //    }
+    //}
 
     /// <summary>
     /// Denne klasse er en fakeklasse af TempSensor klassen. Heri kan vi styre TempGeneratorens værdi. 
@@ -101,14 +100,17 @@ namespace TestLegacy
     /// </summary>
     public class FakeHeater : IHeater
     {
+        public int HeaterIsOn { get; set; } = 0;
+        public int HeaterIsOff { get; set; } = 0;
+
         public void TurnOn()
         {
-
+            ++HeaterIsOn;
         }
 
         public void TurnOff()
         {
-
+            ++HeaterIsOff;
         }
 
         public bool RunSelfTest()
